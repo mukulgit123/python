bd0a102c863804276c1be9e8afa2da2db23d8d35
Mukul Garg <mukul.garg@tothenew.com>
2020-07-23 22:13:13 +0530

    Added average response time script in Basic

---
+++ BASIC/AverageResponseAPI.py
@@ -0,0 +1,23 @@
+def averageResponse(avgResp,count):
+    ar = avgResp/count
+    return(ar)
+
+def main():
+    try:
+        totalResponse = 0
+        count =  0
+        logFile = open("access.log","r")
+        log = logFile.readlines()
+        for x in log:
+            if "/test" in x:
+                k = x.split(" ")
+                totalResponse+=float(k[5])
+                count+=1
+        logFile.close()
+        ar = averageResponse(totalResponse,count)
+        print("Average response time for test api is: ",ar)
+    except Exception as e:
+        print(e)
+
+if __name__=='__main__':
+    main()
