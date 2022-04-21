163241c99180a5be5c9897c4ea3c709b38f07163
mukulgit123 <mukulgit123@users.noreply.github.com>
2022-04-21 13:18:12 +0000

    autopep8 action fixes

--- PUZZLES/SecondHighestScore.py
+++ PUZZLES/SecondHighestScore.py
@@ -7,14 +7,16 @@ if __name__ == '__main__':
     n = int(input())
     arr = map(int, input().split())
     sortArr = list(sorted(arr))
-    secondHighest = -100 # Had to initialize 
-    if n >= 2 and n <= 10 and sortArr[n-1] <= 100 and sortArr[n-1] >= -100: # Exit when constraints are not met for the
+    secondHighest = -100  # Had to initialize
+    if n >= 2 and n <= 10 and sortArr[n - 1] <= 100 and sortArr[n -
+                                                                1] >= -100:  # Exit when constraints are not met for the
         # highest score
-        highestScore = sortArr[n-1]
+        highestScore = sortArr[n - 1]
     else:
         exit()
     for score in sortArr:
-        if score >= 100 and score <= -100: # Exit when constraints are not met for remaining scores
+        if score >= 100 and score <= - \
+                100:  # Exit when constraints are not met for remaining scores
             exit()
         if score < highestScore and score > secondHighest:
             secondHighest = score
