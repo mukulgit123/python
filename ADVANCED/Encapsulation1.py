b58cf0f598c02db60150c8bc1e7ba36aac31f08d
Mukul Garg <mukul.garg@ymail.com>
2024-02-24 17:26:47 +0000

    Encapsulation1

---
+++ ADVANCED/Encapsulation1.py
@@ -0,0 +1,36 @@
+class Person:
+    def __init__(self, name, age, gender):
+        self.__name = name # Two underscores set the attributes to private
+        self.__age = age
+        self.__gender = gender
+
+    @property
+    def Name(self):
+        return self.__name
+
+    @Name.setter
+    def Name(self, value):
+        self.__name = value
+
+    @property
+    def Age(self):
+        return self._age
+
+    @Age.setter
+    def Age(self, value):
+        if value >= 0:
+            self._age = value
+        else:
+            print("Age cannot be negative. Setting age to 0.")
+
+    @staticmethod
+    def mymethod():
+        print("Hello World!!")
+
+Person.mymethod() # For calling a method of a class without instantiating an object of the class.
+p1 = Person("Mike", 29, 'm')
+print(p1.Name)
+p2 = Person("Not here", -1, 'm')
+print(p2.Name)
+p2.Age = -1
+print(p2.Name)
