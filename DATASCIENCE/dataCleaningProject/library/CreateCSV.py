7743ef6de2b0c2736b3eacdee40c0c71671472b8
mukulgit123 <mukulgit123@users.noreply.github.com>
2024-03-01 22:35:13 +0000

    autopep8 action fixes

--- DATASCIENCE/dataCleaningProject/library/CreateCSV.py
+++ DATASCIENCE/dataCleaningProject/library/CreateCSV.py
@@ -1,13 +1,14 @@
 import pandas as pd
 
+
 class CreateCSV:
-    def __init__(self, person , filename, recordcount):
+    def __init__(self, person, filename, recordcount):
         '''This class is used to create a csv file from the data provided'''
         self.person = person
         self.filename = filename
         self.recordcount = recordcount
         self.write_to_csv()
-    
+
     def write_to_csv(self):
         '''This function is used to write the data to a csv file'''
         persons = []
@@ -15,11 +16,18 @@ class CreateCSV:
             for i in range(self.recordcount):
                 person_generator = self.person.generate_person()
                 for person_data in person_generator:
-                    cleaned_record = tuple(s.replace('\n', '') for s in person_data)
+                    cleaned_record = tuple(s.replace('\n', '')
+                                           for s in person_data)
                     persons.append(cleaned_record)
-            df = pd.DataFrame(persons, columns=['Name', 'Address', 'Email', 'Text', 'Phone Number', 'Country'])
+            df = pd.DataFrame(
+                persons,
+                columns=[
+                    'Name',
+                    'Address',
+                    'Email',
+                    'Text',
+                    'Phone Number',
+                    'Country'])
             df.to_csv(self.filename, index=False)
         except Exception as e:
             raise e
-        
-
